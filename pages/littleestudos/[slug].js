b34ed6638cbea9ba6d3c41c728b93fbe1fd0177e
Jamil Filho <jamilf.alien@gmail.com>
2021-02-26 18:53:52 -0300

    chore: atualizando as páginas do #LittleEstudos

---
+++ pages/littleestudos/[slug].js
@@ -0,0 +1,121 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import SiteHeader from '../../components/Header'
+import SiteFooter from '../../components/Footer'
+import { getStudyBySlug, getAllStudys } from '../../lib/studyAPI'
+import markdownToHtml from '../../lib/markdownToHtml'
+import markdownStyles from '../../components/markdown-styles.module.css'
+import { format } from 'date-fns'
+import parseISO from 'date-fns/parseISO'
+import ShareButtons from '../../components/Sharer'
+import { Comment } from '../../components/Comment'
+import { Button } from '../../components/Button'
+import { FiDownload } from 'react-icons/fi'
+
+export default function StudyPage({post}) {
+    const router = useRouter()
+    const studyDate = parseISO(post.date)
+    const formattedDate = format(studyDate, 'dd/MM/yyyy')
+
+    console.log(post)
+    
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage statusCode={404} />
+    }
+    return(
+        <>
+        <Head>
+          <title>{post.title} | Little Son</title>
+        </Head>
+        <SiteHeader pageTitle="LittleEstudos" pageLink="/littleestudos">
+          <span className="text-lg">{post.title}</span>
+        </SiteHeader>
+
+        <main className="w-5/6 md:w-3/6 mx-auto mt-20 pt-10">
+          <article className="md:w-4/5 mx-auto my-12">
+            <header className="header min-h-60 pt-12 pb-20 mb-12">
+                <div className="text-center md:text-left mb-12">
+                    <h1 className="text-3xl md:text-4xl mb-4">{post.title}</h1>
+                    <h2 className="text-lg md:text-xl leading-normal">{post.excerpt}</h2>
+                </div>
+                <div className="w-full text-center md:text-left">
+                  <span className="mr-2"><strong>Escrito por:</strong> {post.author.name}</span> &bull; 
+                  <time className="mx-2" dateTime={post.date}>{formattedDate}</time>
+                </div>
+            </header>
+
+            <section className="mb-6">
+              <div className={markdownStyles['markdown']}  dangerouslySetInnerHTML={{ __html: post.content }}/>
+
+              <div className="w-full mb-6 min-h-32 px-2 py-12 grid grid-cols-3 rounded-lg border-thin border-gray-100 mt-12">
+                <div className="h-full flex justify-center items-center">
+                  <FiDownload className="text-6xl text-gray-400"/>
+                </div>
+
+                <div className="col-span-2 flex flex-col justify-center">
+                  <div className="mb-6">
+                    <p className="text-xl font-bold mb-4 leading-tight">{post.supportMaterial.title}</p>
+                    <p className="text-base">Baixe, gratuitamente, o material de apoio e se aprofunde no estudo.</p>
+                  </div>
+
+                  <div className="w-full lg:w-1/2">
+                  <Button ariaLabel="Baixar PDF">
+                    Baixar PDF
+                  </Button>
+                  </div>
+                </div>
+
+              </div>
+            </section>
+
+            <footer className="pt-6 mt-6">
+              <ShareButtons />
+              
+              <section className="mt-12 md:mt-16">
+                <Comment href={`littleestudos/${post.slug}`}/>
+              </section>
+            </footer>
+          </article>
+        </main>
+        <SiteFooter />
+        </>
+    )
+}
+
+export async function getStaticProps({ params }) {
+    const post = getStudyBySlug(params.slug, [
+      'title',
+      'slug',
+      'date',
+      'excerpt',
+      'author',
+      'supportMaterial',
+      'content'
+    ])
+    
+    const content = await markdownToHtml(post.content || '')
+
+    return {
+      props: {
+        post: {
+          ...post,
+          content,
+        },
+      },
+    }
+}
+
+export async function getStaticPaths() {
+    const posts = getAllStudys(['slug'])
+  
+    return {
+      paths: posts.map((post) => {
+        return {
+          params: {
+            slug: post.slug,
+          },
+        }
+      }),
+      fallback: false,
+    }
+}
--- pages/littleestudos/index.js
+++ pages/littleestudos/index.js
@@ -1,89 +1,49 @@
-import Link from 'next/link'
+import Head from 'next/head'
+import {TextCard} from '../../components/Card'
+import SiteFooter from '../../components/Footer'
 import SiteHeader from '../../components/Header'
-import {Button} from '../../components/Button'
-import { FiExternalLink, FiHeart, FiBookmark } from "react-icons/fi"
+import { getAllStudys } from '../../lib/studyAPI'
 
-export default function HomePage() {
+export default function HomePage({  allPosts  }) {
+    const posts = allPosts
     return(
         <>
+        <Head>
+            <title>LittleEstudos | Little Son</title>
+        </Head>
         <SiteHeader pageTitle="Little Son" pageLink="/">
-            <span className="text-lg"> / LittleDevocional</span>
+            <span className="text-lg">LittleEstudos</span>
         </SiteHeader>
 
         <main className="w-4/6 md:w-3/6 mx-auto mt-32 pt-10">
             <section className="header h-60">
                 <div className="mt-8">
-                    <h1 className="text-4xl">#LittleDevocional</h1>
-                    <h2 className="text-xl">Devocionais diários do projeto Little Son</h2>
+                    <h1 className="text-4xl">#LittleEstudos</h1>
+                    <h2 className="text-xl">Estudos bíblicos para os pequenos conhecerem mais sobre a fé.</h2>
                 </div>
             </section>
 
-            <section className="littledevocional mb-12">
-                <article className="mb-20">
-                    <header className="w-full mb-4 flex flex-col">
-                        <h3 className="w-full text-2xl font-bold">
-                            <Link href=''>
-                                <a>Aenean lobortis felis ac est fringilla</a>
-                            </Link>
-                        </h3>
-                        <Link href="/littledevocional">
-                            <a className="py-2 text-gray-500 font-bold">#LittleDevocional</a>
-                        </Link>
-                    </header>
-
-                    <section className="mb-6">
-                        <p className="text-base text-gray-600 leading-tight">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium tellus nec lectus auctor facilisis et id turpis. Mauris ut maximus tortor, eget sollicitudin dui. Phasellus sed ex tortor. In justo metus, euismod ac mattis a, pretium sed quam. Nullam in ultrices lorem, eget ultrices dui. Aenean lobortis felis ac est fringilla, et pharetra nibh ullamcorper. Vivamus congue metus nisi, ac porttitor augue accumsan sed. Proin aliquam semper metus, sed tempus turpis tempus quis.</p>
-                    </section>
-                    <footer className="w-full">
-                        <div className="w-full lg:w-1/3 grid grid-cols-3 gap-4">
-                            <Button>
-                                <FiExternalLink />
-                            </Button>
-
-                            <Button>
-                                <FiHeart />
-                            </Button>
-
-                            <Button>
-                                <FiBookmark />
-                            </Button>
-                        </div>
-                    </footer>
-                </article>
-
-                <article className="mb-20">
-                    <header className="w-full mb-4 flex flex-col">
-                        <h3 className="w-full text-2xl font-bold">
-                            <Link href=''>
-                                <a>Proin aliquam semper metus, sed tempus turpis tempus qui</a>
-                            </Link>
-                        </h3>
-                        <Link href="">
-                            <a className="py-2 text-gray-500 font-bold">#LittleDevocional</a>
-                        </Link>
-                    </header>
-
-                    <section className="mb-6">
-                        <p className="text-base text-gray-600 leading-tight">Aenean lobortis felis ac est fringilla, et pharetra nibh ullamcorper. Vivamus congue metus nisi, ac porttitor augue accumsan sed. Proin aliquam semper metus, sed tempus turpis tempus quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium tellus nec lectus auctor facilisis et id turpis.</p>
-                    </section>
-                    <footer className="w-full">
-                        <div className="w-full lg:w-1/3 grid grid-cols-3 gap-4">
-                            <Button>
-                                <FiExternalLink />
-                            </Button>
-
-                            <Button>
-                                <FiHeart />
-                            </Button>
-
-                            <Button>
-                                <FiBookmark />
-                            </Button>
-                        </div>
-                    </footer>
-                </article>
+            <section className="littleestudos mb-12">
+            {posts.map((post) => (
+                <TextCard key={post.slug} title={post.title} slug={`/littleestudos/${post.slug}`} excerpt={post.excerpt} buttonText="Ler estudo"/>
+            ))}
             </section>
         </main>
+
+        <SiteFooter />
         </>
     )
+}
+
+export async function getStaticProps() {
+    const allPosts = getAllStudys([
+      'title',
+      'slug',
+      'excerpt',
+      'date'
+    ])
+
+    return {
+      props: { allPosts  },
+    }
 }
