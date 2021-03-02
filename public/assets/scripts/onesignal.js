46618e0ecafb6198144ce81b0679b50b919c60fa
Jamil Filho <jamilf.alien@gmail.com>
2021-03-02 13:15:55 -0300

    feat: scripts OneSignal

---
+++ public/assets/scripts/onesignal.js
@@ -0,0 +1,7 @@
+{/* <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script> */}
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+OneSignal.init({
+    appId: "345f2fb5-715a-470c-acb2-201fa5b8219f",
+});
+});
