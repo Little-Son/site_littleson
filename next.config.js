3d8f0806cf4d7f5bd57931aec5e60e50dbd10f48
Jamil Filho <jamilf.alien@gmail.com>
2021-03-11 12:15:26 -0300

    chore: atualizando a rota da loja no next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,11 @@ module.exports = withPWA({
         destination: 'https://www.amazon.com.br/?&_encoding=UTF8&tag=littleson-20&linkCode=ur2&linkId=6436fe63ac80e6d638b722b4af8fbb49&camp=1789&creative=9325',
         permanent: true,
       },
+      {
+        source: `/loja`,
+        destination: 'https://www.colab55.com/@umlittleson/sketchbooks?aff=x5w8c6',
+        permanent: true,
+      },
     ]
   },
 })
