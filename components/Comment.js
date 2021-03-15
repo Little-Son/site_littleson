45a8b701e891fdf7d0667d030f28e98bbef63841
Jamil Filho <jamilf.alien@gmail.com>
2021-03-14 21:25:53 -0300

    revert: removendo o next-auth

--- components/Comment.js
+++ components/Comment.js
@@ -1,24 +1,14 @@
-import { FacebookProvider, Comments, CommentsCount } from 'react-facebook';
-import {useSession} from 'next-auth/client'
+import { FacebookProvider, Comments, CommentsCount } from 'react-facebook'
 
 export function Comment(props) {
-  const [ session ] = useSession()
   const appID = "438630510651505"
   
   return(
     <section className="mt-28">
-      {!session && (
-        <div className="mb-28">
-          <p class="text-2xl font-bold mb-2">O que você achou?</p>
-          <p className="text-lg">Conta pra gente nos comentários 😊😉</p>
-        </div>
-      )}
-      {session && (
-        <div className="mb-20">
-          <p class="text-2xl font-bold mb-2">Hey, {session.user.name}. O que você achou?</p>
-          <p className="text-lg">Conta pra gente nos comentários 😊😉</p>
-        </div>
-      )}
+      <div className="mb-28">
+        <p class="text-2xl font-bold mb-2">O que você achou?</p>
+        <p className="text-lg">Conta pra gente nos comentários 😊😉</p>
+      </div>
       <FacebookProvider appId={appID}>
         <Comments href={`https://littleson.com.br/${props.href}`} width="100%" />
       </FacebookProvider>
